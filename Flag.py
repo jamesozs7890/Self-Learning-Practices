1223412d85365f4f4150ac6edbf298c22da718af
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2023-06-06 14:59:08 +0800

    Adding hex to bytes function

--- Flag.py
+++ Flag.py
@@ -19,4 +19,12 @@ def flag2():
     # shortcut
     print("".join(chr(o) for o in ords))
 
-flag2()
+
+def hexToBytes():
+    hexCode = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+
+    bytesCode = bytes.fromhex(hexCode)
+    print(bytesCode)
+
+
+hexToBytes()
